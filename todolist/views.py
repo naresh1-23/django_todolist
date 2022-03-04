cc38ba048e4e54a8741dd468dc0ca1301ae915b2
naresh1-23 <nareshtamang20000@gmail.com>
2022-03-04 16:59:12 +0545

    full

--- todolist/views.py
+++ todolist/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-
+from django.contrib import messages
 
 def index(request):
     if request.method == 'POST':
         task = models.Post()
         task.task = request.POST['text']
-        task.save()
-        return render(request, 'index.html')
+        if models.Post.objects.filter(task = task).exists():
+            messages.info(request, 'task already exist')
+            return redirect('index')
+        else:
+            task.save()
+            messages.info(request, 'task is added')
+            return render(request, 'index.html')
     return render(request, 'index.html')
 
 def task(request):
     work = models.Post.objects.all()
     return render(request, 'task.html', {'work': work})
-# Create your views here.
+
+def detail(request, pick):
+    work = models.Post.objects.get(id = pick )
+    return render(request, 'detail.html', {'works':work})
